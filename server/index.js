068a178b7d2bf5cd5959f4435fd8a8601ea00d21
bellobambo <bellobambo21@gmail.com>
2024-05-09 01:00:48 +0100

    activity

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
-import { createServer } from "http"
 import { Server } from "socket.io"
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from "url"
 
-const httpServer = createServer()
 
-const io = new Server(httpServer, {
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const PORT = process.env.PORT || 3500
+const app = express()
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+const expressServer = app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
+
+const io = new Server(expressServer, {
     cors: {
         origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
     }
@@ -12,10 +25,16 @@ const io = new Server(httpServer, {
 io.on('connection', socket => {
     console.log(`User ${socket.id} connected`)
 
+    // Upon Connection - only to User
+    socket.emit('message' , 'welcome to chat app')
+
+    // Upon Connection - only to User
+socket.broadcast.emit('message' ,`User ${socket.id.substring(0, 5)} connected` )
+
+
+
     socket.on('message', data => {
         console.log(data)
         io.emit('message', `${socket.id.substring(0, 5)}: ${data}`)
     })
 })
-
-httpServer.listen(3500, () => console.log('listening on port 3500'))
